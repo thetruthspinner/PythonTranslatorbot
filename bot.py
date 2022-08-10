f64b04aa1d25f04f29d23b0dcbe67dc2045780b1
thetruthspinner <40661589+thetruthspinner@users.noreply.github.com>
2022-08-10 15:11:29 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+from telegram.ext import Updater,MessageHandler,CommandHandler,Filters
+from google_trans_new import google_translator 
+import os
+
+BOT_TOKEN = '5462330526:AAHVnNoLYJBULOmDZehUZlP-j5DUybyfwLY'
+
+updater = Updater(BOT_TOKEN,use_context = True )
+
+def start(updater,context):
+ updater.message.reply_text('hi iam google translater I can translate any language to kannada join for more updates @lntechnical ')
+ 
+def echo(updater,context):
+ usr_msg =updater.message.text
+ translator=google_translator()
+ translate_text=translator.translate(usr_msg,lang_tgt='kn')
+ updater.message.reply_text(translate_text)
+ 
+dp =updater.dispatcher.add_handler
+dp(CommandHandler('start',start))
+dp(MessageHandler(Filters.text,echo))
+
+updater.start_polling()
+updater.idle()
